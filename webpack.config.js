8f75572a145c12efa5c9e7bbfac8f43bdcb4abcb
Alex Harding <alex.harding@whale-net.net>
2018-10-08 19:57:53 -0400

    added package info, ew

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/* 
+ * Alex Harding <alex.harding@whale-net.net>
+ * setup files cloned from school project
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './wizsite/js/main.jsx',
+  output: {
+    path: path.join(__dirname, '/wizsite/static/js/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        // Test for js or jsx files
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          // Convert ES6 syntax to ES5 for browser compatibility
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
---
+++ wizsite/js/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from'react-dom';
+
+ReactDom.render(
+  <Index eventually="setup-things-in-here" />,
+  //Select reactEntry as the element we modify
+  document.getElementById('reactEntry'),  
+);
