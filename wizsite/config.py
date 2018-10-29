c08adda62d6c4c91a7969345779eb26a0819b9a0
alhardng <alex.harding@whale-net.net>
2018-10-29 18:46:25 -0400

    added config information

--- wizsite/config.py
+++ wizsite/config.py
@@ -4,3 +4,12 @@ Wizsite config.
 import os
 
 APPLICATION_ROOT = '/'
+
+# Key for encrypting cookies.
+SECRET_KEY = b''
+
+# Database information
+DATABASE_NAME = 'wizsite_dev'
+DATABASE_USERNAME = 'wiz'
+DATABASE_HOST = 'localhost'
+DATABASE_PASSWORD = 'password'
