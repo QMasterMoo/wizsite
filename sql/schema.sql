13ed5016e5b8ab613238822c7d087c27f0455161
alhardng <alex.harding@whale-net.net>
2018-10-29 18:22:11 -0400

    added blog capability to schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,3 +6,38 @@ CREATE TABLE Users (
     PRIMARY KEY(uid),
     UNIQUE(username)
 );
+
+CREATE TABLE Blog (
+    bid INTEGER AUTO_INCREMENT,
+    author_id INTEGER NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    visible BOOLEAN DEFAULT 0 NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(bid),
+    FOREIGN KEY(author_id) REFERENCES Users(uid)
+);
+
+CREATE TABLE Content (
+    tid INTEGER AUTO_INCREMENT,
+    bid INTEGER NOT NULL,
+    author_id INTEGER,
+    content VARCHAR(21844),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY(tid),
+    FOREIGN KEY(bid) REFERENCES Blog(bid),
+    FOREIGN KEY(author_id) REFERENCES Users(uid)
+);
+
+CREATE VIEW LatestBlogs AS (
+    SELECT b.title, b.posted, c.content, c.created as edited
+    FROM Blog b
+    INNER JOIN (
+        SELECT MAX(c.tid) as tid, c.bid
+        FROM Content c
+        GROUP BY c.bid
+    ) lid ON b.bid = lid.bid
+    INNER JOIN Content c 
+    ON lid.tid = c.tid
+);
+
