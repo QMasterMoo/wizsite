463cbbe46a2b677b6c5cfaeda7a7e21855b8c74c
alhardng <alex.harding@whale-net.net>
2018-10-29 21:03:00 -0400

    header file config information

--- wizsite/config.py
+++ wizsite/config.py
@@ -2,6 +2,7 @@
 Wizsite config.
 """
 import os
+import datetime
 
 APPLICATION_ROOT = '/'
 
@@ -13,3 +14,8 @@ DATABASE_NAME = 'wizsite_dev'
 DATABASE_USERNAME = 'wiz'
 DATABASE_HOST = 'localhost'
 DATABASE_PASSWORD = 'password'
+
+# Header information
+TWITCH_NAME = ''
+HEADER_UPDATE_INTERVAL_SECONDS = 60
+TWITCH_CLIENT_ID = ''
