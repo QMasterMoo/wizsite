8c4bb62b9ba935046a4e3eed960fe038fdd7122e
alhardng <alex.harding@whale-net.net>
2018-10-29 20:45:30 -0400

    added way to update context with header info

--- wizsite/views/index.py
+++ wizsite/views/index.py
@@ -6,10 +6,13 @@ Main endpoint for wizsite.
 import flask
 import wizsite
 
+from wizsite.util import update_header_dict
+
 @wizsite.app.route('/', methods=['GET'])
 def get_index():
     """
     Return index template.
     """
     context = {}
-    return flask.render_template("temp_index.html", **context)
+    update_header_dict(context)
+    return flask.render_template("index.html", **context)
